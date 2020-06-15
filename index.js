06e8c6dcdacdae81e003b20e79aa8ca6eae4d870
SohamPing <52289667+SohamPing@users.noreply.github.com>
2020-06-15 17:40:52 +0530

    Initial Game with Button

---
+++ index.js
@@ -0,0 +1,53 @@
+// DICE 1
+var randomNumber1;
+var flag=0;
+randomNumber1 = Math.floor(6*Math.random()+1);
+console.log(randomNumber1);
+switch(randomNumber1){
+    
+   case(1): document.querySelector(".dice .img1").setAttribute("src","images/dice1.png" );
+            break;
+    case(2): document.querySelector(".dice .img1").setAttribute("src","images/dice2.png" );
+            break;
+    case(3): document.querySelector(".dice .img1").setAttribute("src","images/dice3.png" );
+            break;
+    case(4): document.querySelector(".dice .img1").setAttribute("src","images/dice4.png" );
+            break;
+    case(5): document.querySelector(".dice .img1").setAttribute("src","images/dice5.png" );
+            break;
+    case(6): document.querySelector(".dice .img1").setAttribute("src","images/dice6.png" );
+            break;
+}  
+//////////////// 
+// DICE 2
+var randomNumber2;
+randomNumber2 = Math.floor(6*Math.random()+1);
+console.log(randomNumber1);
+switch(randomNumber2){
+    
+    case(1): document.querySelector(".dice .img2").setAttribute("src","images/dice1.png" );
+            break;
+    case(2): document.querySelector(".dice .img2").setAttribute("src","images/dice2.png" );
+            break;
+    case(3): document.querySelector(".dice .img2").setAttribute("src","images/dice3.png" );
+            break;
+    case(4): document.querySelector(".dice .img2").setAttribute("src","images/dice4.png" );
+            break;
+    case(5): document.querySelector(".dice .img2").setAttribute("src","images/dice5.png" );
+            break;
+    case(6): document.querySelector(".dice .img2").setAttribute("src","images/dice6.png" );
+            break;
+}  
+//////////////// 
+
+    if(randomNumber1>randomNumber2)
+    {
+        document.querySelector(".container .title").innerHTML="Player 1 WON!";
+    }
+    else if(randomNumber1<randomNumber2){
+        document.querySelector(".container .title").innerHTML="Player 2 WON!";
+    }
+    else{
+        document.querySelector(".container .title").innerHTML="Its a tie!";
+    }   
+
